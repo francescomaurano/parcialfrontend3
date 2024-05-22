e0b96fc51095cd550021caa746f29d136128d776
francescomaurano <francescomaurano10@gmail.com>
2024-05-22 20:05:53 -0300

    primera subida

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+// src/Card.jsx
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="card">
+      <h2>Estudiante</h2>
+      <p>Detalles del estudiante aquí</p>
+    </div>
+  );
+}
+
+export default Card;
